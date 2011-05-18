7055b942c65eb55e59b63a02bd5ba00cedb30c8f
Pasi Vesterinen <ara001spb@toonsreview.com>
2011-05-18 15:15:26 +0400

    ver0.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oemsoft::Application.config.secret_token = '13745abe7ac0dc0bd25be32cb8eda1eb68f326229c9297815b0af00be6e17ea0407260d18e0fdee7730190eafd8315e7f706141e656192c3cdacc0f4f26e63df'
